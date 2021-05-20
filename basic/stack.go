334dba04e1f6da38ff42db64e4ee8c8c006a3d14
zengyan.yan <308792303@qq.com>
2021-05-20 17:23:16 +0800

    backup

--- basic/stack.go
+++ basic/stack.go
@@ -1,1 +1,53 @@
 package basic
+
+import "fmt"
+
+type Stack struct {
+	base []ElementType
+	top int
+	cap int
+}
+
+func (s *Stack) IsEmpty() bool {
+	// TODO
+	return false
+}
+
+func (s *Stack) IsFull() bool {
+	// TODO
+	return false
+}
+
+func (s *Stack) Capacity() int {
+	// TODO
+	return 0
+}
+
+func (s *Stack) Clear() {
+	// TODO
+}
+
+func (s *Stack) Push(e ElementType) {
+	// TODO
+}
+
+func (s *Stack) Pop() {
+	// TODO
+}
+
+func (s *Stack) Top() ElementType {
+	// TODO
+	return 0
+}
+
+func NewStack(cap int) *Stack {
+	if cap <= 0 {
+		panic(fmt.Sprintf("invalid stack cap: %d", cap))
+	}
+	return &Stack{
+		base: make([]ElementType, 0, cap),
+		top: -1,
+		cap: cap,
+	}
+}
+

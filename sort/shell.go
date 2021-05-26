c4082fb56836e3b3b716bb5383807f667aae4906
zengyan.yan <308792303@qq.com>
2021-05-26 14:47:53 +0800

    sort

--- sort/shell.go
+++ sort/shell.go
@@ -3,7 +3,7 @@ package sort
 func ShellSort(arr []int) {
 	size := len(arr)
 	for inc := size / 2; inc > 0; inc /= 2 {
-		for i := inc; i < size; i += inc {
+		for i := inc; i < size; i++ {
 			tmp := arr[i]
 			j := i - inc
 			for ; j >= 0; j -= inc {
